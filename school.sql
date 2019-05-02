06a661923159a07b27804db4fdec4e1616e02f25
AyanamiSho <2584966924@qq.com>
2019-05-03 01:06:58 +0800

    add distributed

--- school.sql
+++ school.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80015
  File Encoding         : 65001
 
- Date: 03/05/2019 01:00:17
+ Date: 03/05/2019 01:06:41
 */
 
 SET NAMES utf8mb4;
@@ -22,9 +22,9 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `class`;
 CREATE TABLE `class`  (
-  `class_id` int(255) NOT NULL,
-  `class_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `college_id` int(11) NULL DEFAULT NULL,
+  `class_id` int(255) NOT NULL COMMENT '班级id 主键',
+  `class_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '班级名',
+  `college_id` int(11) NULL DEFAULT NULL COMMENT '连接学院表的外键',
   PRIMARY KEY (`class_id`) USING BTREE,
   INDEX `ctoc`(`college_id`) USING BTREE,
   CONSTRAINT `ctoc` FOREIGN KEY (`college_id`) REFERENCES `college` (`college_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
@@ -35,21 +35,21 @@ CREATE TABLE `class`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `college`;
 CREATE TABLE `college`  (
-  `college_id` int(11) NOT NULL,
-  `college_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `college_id` int(11) NOT NULL COMMENT '学院id 主键',
+  `college_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '学院名称',
   PRIMARY KEY (`college_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '学院表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Table structure for student
 -- ----------------------------
 DROP TABLE IF EXISTS `student`;
 CREATE TABLE `student`  (
-  `student_id` int(255) NOT NULL,
-  `student_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `class_id` int(11) NULL DEFAULT NULL,
-  `student_sex` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `student_age` int(255) NULL DEFAULT NULL,
+  `student_id` int(255) NOT NULL COMMENT '学生id 主键',
+  `student_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '学生名字',
+  `class_id` int(11) NULL DEFAULT NULL COMMENT '连接学生表的外键',
+  `student_sex` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '学生性别',
+  `student_age` int(255) NULL DEFAULT NULL COMMENT '学生年龄',
   PRIMARY KEY (`student_id`) USING BTREE,
   INDEX `stoc`(`class_id`) USING BTREE,
   CONSTRAINT `stoc` FOREIGN KEY (`class_id`) REFERENCES `class` (`class_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
